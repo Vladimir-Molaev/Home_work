83d5d6a96481ea3779575d44fe5081e4334b9b41
Vladimir <i31209@yandex.ru>
2022-12-25 18:45:35 +0300

    Home work 6.2 completed successfully

--- Home_work_6/Home_work_6.2/Program.cs
+++ Home_work_6/Home_work_6.2/Program.cs
@@ -4,3 +4,26 @@
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
+
+
+// y = 5 * x + 2    y = 5 * -0.5 + 2 = -0.5 
+//     k2      b2   k1      b1  k2      b2        k1 - k2 = b2-b1  
+// y = 9 * x + 4    5 * x + 2 = 9 * x + 4         5*x-9*x = 4 - 2       -4 * x = 2      x = 2/-4=-0.5
+ 
+Console.Write("Введите значения b1, k1, b2, k2: ");
+string[] s = Console.ReadLine().Split();            // Парсим введенную строку со значениями
+int b1 = int.Parse(s[0]);                           //
+int k1 = int.Parse(s[1]);                           //
+int b2 = int.Parse(s[2]);                           //
+int k2 = int.Parse(s[3]);                           //
+
+double k = (k1 - k2);
+Console.WriteLine("k = " + k);
+double b = b2 - b1;
+Console.WriteLine("b = " + b);
+double x = Convert.ToDouble(b/k);
+double y = k1*x + b1;
+
+Console.WriteLine("x = " + x);
+Console.WriteLine("y = " + y);
+
