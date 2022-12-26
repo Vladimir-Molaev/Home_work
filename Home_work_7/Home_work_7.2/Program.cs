f70f724a3179ddd5c20273a12aef31845d3be4ae
Vladimir <i31209@yandex.ru>
2022-12-26 22:40:36 +0300

    Home work 7.2 finally version

--- Home_work_7/Home_work_7.2/Program.cs
+++ Home_work_7/Home_work_7.2/Program.cs
@@ -5,29 +5,35 @@
 1 4 7 2
 5 9 2 3
 8 4 2 4
+[1,7] -> такого числа в массиве нет
 */
-Random rnd = new Random();
-int m = rnd.Next(1,11);
-int n = rnd.Next(1,11);
-double[,] array = new double[m, n];
+Console.Write("Введите позицию в двухмерном массиве: ");
+string[] s = Console.ReadLine().Split();                    // ввод позиции в массиве
+int m = int.Parse(s[0]);
+int n = int.Parse(s[1]);
 
-void GeneratingInArray(int m, int n)
+Random rnd = new Random();                                  // генерируем начальные размеры массива
+int i = rnd.Next(1, 10);                                    // ограничил величинами 9х9
+int j = rnd.Next(1, 10);
+int [,] array = new int[i, j];
+
+void GeneratingInArray(int row, int column)                 // генерируем массив
 {
     Random rnd = new Random();
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < n; j++ )
+        for (int j = 0; j < column; j++ )
         {
             array[i, j] = rnd.Next(1, 100);
         }
     }
 }
 
-void PrintArray(int m, int n)
+void PrintArray(int row, int column)                           // печатаем на экран
 {
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < column; j++)
         {
             Console.Write(array[i, j] + " ");
         }
@@ -35,6 +41,18 @@ void PrintArray(int m, int n)
     }
 }
 
-GeneratingInArray(m, n);
-PrintArray(m, n);
+void CheckingTheElement(int m, int n)                           // проверяем введенную позицию на наличие в массиве
+{
+    if (m >= i || n >= j)
+    {
+        Console.WriteLine($"array[{m},{n}] - такой позиции не в массиве");
+    }
+    else
+    {
+        Console.WriteLine($"array[{m},{n}] = {array[m, n]}");
+    }
+}
 
+GeneratingInArray(i, j);
+PrintArray(i, j);
+CheckingTheElement(m, n);
