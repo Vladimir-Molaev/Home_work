3852256d1c6db615fd3c63a2d17bb7634b46816d
Vladimir <i31209@yandex.ru>
2023-01-02 17:28:41 +0300

    Home work 9.1 finally version

---
+++ Home_work_9/Home_work_9.1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 1: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Random rnd = new Random();
+int N = rnd.Next(10);
+Console.WriteLine("N = " + N);
+int RecursionFunction(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        Console.Write(n + ", ");
+        return RecursionFunction(n - 1);
+    }
+}
+Console.WriteLine(RecursionFunction(N));
