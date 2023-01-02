0d3215eef276909cb3f87fab2bfcaa232bbe6ed9
Vladimir <i31209@yandex.ru>
2023-01-02 22:36:19 +0300

    Home work 9.2 finally version

---
+++ Home_work_9/Home_work_9.2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Random rnd = new Random();
+int M = rnd.Next(11);
+Console.WriteLine("M = " + M);
+int N = rnd.Next(11, 100);
+Console.WriteLine("N = " + N);
+int count = M;
+int summ  = M;
+int RecursionFunction(int m)
+{
+    if (count == N)
+    {
+        return summ;
+    }
+    else
+    {
+        count++;
+        summ += count;
+        return RecursionFunction(count);
+    }
+}
+Console.WriteLine(RecursionFunction(M));
