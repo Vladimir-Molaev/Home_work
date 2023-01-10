838974b7be0af71b9e0391e7b2ded7cde4f981bb
Vladimir <i31209@yandex.ru>
2023-01-10 17:43:29 +0300

    Home_work 9 successfully completed

--- Home_work_9/Home_work_9.4/Program.cs
+++ Home_work_9/Home_work_9.4/Program.cs
@@ -11,7 +11,7 @@ internal class Program
     private static void Main(string[] args)
     {
         Random rnd = new Random();
-        int size_of_array = 20; //rnd.Next(4, 11);
+        int size_of_array = rnd.Next(4, 11);
         int[,] array = new int[size_of_array, size_of_array];
         int i = 0, j = 0, count = 1, row = 1;
 
@@ -20,7 +20,7 @@ internal class Program
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
-                { Console.Write(array[i, j].ToString("D3") + " "); }
+                { Console.Write(array[i, j].ToString("D2") + " "); }
                 Console.WriteLine();
             }
         }
@@ -34,74 +34,41 @@ internal class Program
         }
 
 
-        while (count < array.GetLength(0) * array.GetLength(1)+1)
+        while (count < array.GetLength(0) * array.GetLength(1)+1) // первое условие
         {
             
             if (j+1 < array.GetLength(1) && array[i, j+1] == 0 && i < row)
             {
-                j++;
-                Console.WriteLine("Выполнение первого условия.");
-                Console.WriteLine($"i = {i}, j = {j}.");   
-
-                StepForward(i, j);
-                
-                PrintArray(array);
-                Console.WriteLine($"i = {i}, j = {j}.");
-                Console.WriteLine();
+                j++;                
+                StepForward(i, j);            
                 continue;
             }
 
             if (i+1 < array.GetLength(0) && array[i+1, j] == 0)
             {
-                i++;
-            
-                Console.WriteLine("Выполнение второго условия.");
-                Console.WriteLine($"i = {i}, j = {j}.");  
-
+                i++;            
                 StepForward(i, j);
-                
-                PrintArray(array);
-                Console.WriteLine($"i = {i}, j = {j}.");
-                Console.WriteLine();
                 continue;
             }
 
             if (j-1 >= 0 && array[i, j-1] == 0)
             {
-                Console.WriteLine("Выполнение третьего условия.");
-                Console.WriteLine($"i = {i}, j = {j}."); 
-
                 j--;
                 StepForward(i, j-1);
-                
-                PrintArray(array);
-                Console.WriteLine($"i = {i}, j = {j}.");
-                Console.WriteLine();
                 continue;
             }
 
             if (i-1 > 0 && array[i-1, j] == 0 )
             {
-                i--;
-                Console.WriteLine("Выполнение четвертого условия.");
-                Console.WriteLine($"i = {i}, j = {j}."); 
-
+                i--;                
                 StepForward(i-1, j);
-
-                PrintArray(array);
-                Console.WriteLine($"i = {i}, j = {j}.");
-                Console.WriteLine();
                 if (array[i-1, j] !=0 )
                 {
-                    row++;
+                    row++; // эта строка необходима для того, чтобы первое условие не выполнялось раньше времени
                 }
                 continue;
             }
-
-
-
         }
-
         PrintArray(array);
     }
 }
