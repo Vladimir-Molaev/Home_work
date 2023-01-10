b342f3c14bdf2dc693c97f1a715596f108c5eae4
Vladimir <i31209@yandex.ru>
2023-01-10 17:40:36 +0300

    Home_work 9 successfully completed

---
+++ Home_work_9/Home_work_9.3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9 (?)
+m = 3, n = 2 -> A(m,n) = 29 (?)
+*/
+Console.Write("Функция Аккермана, введите m и n: ");
+string[] s = Console.ReadLine().Split();
+int m = int.Parse(s[0]);
+int n = int.Parse(s[1]);
+
+double AckermanFunction (double m, double n)
+{
+    if (m == 0)
+    {
+        return n+1;
+    }
+
+    else if (m > 0 && n == 0) 
+    {
+        return (AckermanFunction(m-1, 1));
+    }
+    else
+    {
+        return (AckermanFunction(m-1, AckermanFunction(m, n-1)));
+    }
+}
+Console.WriteLine($"A({m}, {n}) = " + AckermanFunction(m, n));
---
+++ Home_work_9/Home_work_9.4/Program.cs
@@ -0,0 +1,107 @@
+﻿/* Задача 4*. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int size_of_array = 20; //rnd.Next(4, 11);
+        int[,] array = new int[size_of_array, size_of_array];
+        int i = 0, j = 0, count = 1, row = 1;
+
+        void PrintArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                { Console.Write(array[i, j].ToString("D3") + " "); }
+                Console.WriteLine();
+            }
+        }
+        array[i, j] = count;
+        count++;
+
+        void StepForward(int y, int x)
+        {
+            array[i, j] = count;
+            count++;
+        }
+
+
+        while (count < array.GetLength(0) * array.GetLength(1)+1)
+        {
+            
+            if (j+1 < array.GetLength(1) && array[i, j+1] == 0 && i < row)
+            {
+                j++;
+                Console.WriteLine("Выполнение первого условия.");
+                Console.WriteLine($"i = {i}, j = {j}.");   
+
+                StepForward(i, j);
+                
+                PrintArray(array);
+                Console.WriteLine($"i = {i}, j = {j}.");
+                Console.WriteLine();
+                continue;
+            }
+
+            if (i+1 < array.GetLength(0) && array[i+1, j] == 0)
+            {
+                i++;
+            
+                Console.WriteLine("Выполнение второго условия.");
+                Console.WriteLine($"i = {i}, j = {j}.");  
+
+                StepForward(i, j);
+                
+                PrintArray(array);
+                Console.WriteLine($"i = {i}, j = {j}.");
+                Console.WriteLine();
+                continue;
+            }
+
+            if (j-1 >= 0 && array[i, j-1] == 0)
+            {
+                Console.WriteLine("Выполнение третьего условия.");
+                Console.WriteLine($"i = {i}, j = {j}."); 
+
+                j--;
+                StepForward(i, j-1);
+                
+                PrintArray(array);
+                Console.WriteLine($"i = {i}, j = {j}.");
+                Console.WriteLine();
+                continue;
+            }
+
+            if (i-1 > 0 && array[i-1, j] == 0 )
+            {
+                i--;
+                Console.WriteLine("Выполнение четвертого условия.");
+                Console.WriteLine($"i = {i}, j = {j}."); 
+
+                StepForward(i-1, j);
+
+                PrintArray(array);
+                Console.WriteLine($"i = {i}, j = {j}.");
+                Console.WriteLine();
+                if (array[i-1, j] !=0 )
+                {
+                    row++;
+                }
+                continue;
+            }
+
+
+
+        }
+
+        PrintArray(array);
+    }
+}
